f244fb84ac016a69d2791329c464955a209b8227
max <meison98@bk.ru>
2019-04-06 19:30:34 +0300

    2 - сделал

--- Maxim_Mozhorov/2.js
+++ Maxim_Mozhorov/2.js
@@ -1,3 +1,22 @@
 function convert(str) {
+    let res = '[';
+    let counter = 0;
+    let i = 0;
+    for (let key in str) {
+        counter++;
+    }
 
+    for (let key in str) {
+         res += '[' + '"' + key.toString() + '", ' + '"' + str[key].toString() + '"'+']';
+         i++;
+         if (counter!=i){
+             res += ', '
+         }
+    }
+
+    res += ']';
+    return res;
 }
+
+let a = {name: 'Jeremy', age: 24, role: 'Software Engineer'}
+console.log(convert(a));
