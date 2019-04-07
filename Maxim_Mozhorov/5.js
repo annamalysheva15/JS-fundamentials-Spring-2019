96026ee660fc7a80499e902efe5cf6323e49665d
max <meison98@bk.ru>
2019-04-07 22:47:38 +0300

    5 - сделал

---
+++ Maxim_Mozhorov/5.js
@@ -0,0 +1,26 @@
+function stringExpansion(str) {
+    let res = '';
+    let mas_num = [];
+    let mas_sym = [];
+    for (let i = 0; i < str.length; i++){
+        if (!isNaN(str[i]) && isNaN(str[i+1]) && (i+1)<str.length){//if number
+            mas_num[i] = parseInt(str[i]);
+            for (let j = 0; j<mas_num[i]; j++){
+                res += str[i+1];
+            }
+        }
+        else if (isNaN(str[i]) && isNaN(str[i+1]) && (i+1)<str.length){
+            res += str[i];
+        }
+        else if(isNaN(str[i]) && isNaN(str[i-1])){
+            res += str[i];
+        }
+    }
+    console.log(res);
+}
+
+stringExpansion('3D2a5d2f');
+stringExpansion('3d332f2a');
+stringExpansion('abcde');
+stringExpansion('ZS3f5g');
+stringExpansion('v4eB');
