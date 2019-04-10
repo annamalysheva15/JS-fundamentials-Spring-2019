90f21539247432acf8ce30a01f3f6f7079f977eb
max <meison98@bk.ru>
2019-04-10 12:45:12 +0300

    3 - убрал преобразовние в верхний регистр у первого слова, т.к. оно может быть в нижнем регистре

--- Maxim_Mozhorov/3.js
+++ Maxim_Mozhorov/3.js
@@ -1,7 +1,7 @@
 function toCamelCase(str) {
     let buffer_str = str.replace(/\_/g,'-');
     buffer_str = buffer_str.split('-');
-    for(let i = 0; i < buffer_str.length;i++){
+    for(let i = 1; i < buffer_str.length;i++){
         buffer_str[i] = buffer_str[i][0].toUpperCase() + buffer_str[i].slice(1);
     }
     buffer_str = buffer_str.join('');
