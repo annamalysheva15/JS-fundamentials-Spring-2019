85500c868b3118cd3d20d2c8b6eb767cc85e40be
Cidojo <cidell@mail.ru>
2019-04-15 00:05:44 +0300

    removing comments at task-9

--- task-9.js
+++ task-9.js
@@ -1,7 +1,6 @@
 // 9. Task 9
 
 const countDown = (start) => {
-  debugger
   if (isNaN(start)) {
     return NaN;
   } else if (+start) {
@@ -9,5 +8,7 @@ const countDown = (start) => {
   }
   return console.log(start);
 }
+
 // test
+
 countDown(3);
